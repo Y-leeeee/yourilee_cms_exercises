84d44a65a0151f053b4688d0da3e195397c6fc2c
Youri Lee <yourilee@Youris-Air.lan>
2024-11-15 15:49:02 +0100

    Added phpbook as a regular folder

---
+++ uppgift4/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP & MySQL</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
+  </head>
+  <body>
+  <?php
+echo "here i write uppgift4 code"
+?>
+  </body>
+</html>
