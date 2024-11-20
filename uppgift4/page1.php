625c18697abe0fa1dd41a48b6daafc95e3a12371
Youri Lee <yourilee@Youris-Air.lan>
2024-11-20 12:37:19 +0100

    Add wordprss and write code on page1

--- uppgift4/page1.php
+++ uppgift4/page1.php
@@ -4,6 +4,14 @@
 <?php
 echo "Today is " . date("l, ") . date("Y.m.d") . "
 <br>";
+    $dag = date("d");
+    if($dag % 2 == 0){
+        echo date("Y.m.d") . " is an even date."; 
+    }
+    else{
+        echo date("Y.m.d") . " is an odd date";
+    }
+
     
 ?>    
 </body>
